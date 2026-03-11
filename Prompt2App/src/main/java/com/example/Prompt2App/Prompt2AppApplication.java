4caf363da3fbb92fdb6f8d31a7dcefbdc585dfee
Lorika Kalbhor <66677440+LorikaKalbhor18@users.noreply.github.com>
2026-03-11 21:54:40 +0530

    Entity

---
+++ Prompt2App/src/main/java/com/example/Prompt2App/Prompt2AppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Prompt2App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prompt2AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prompt2AppApplication.class, args);
+	}
+
+}
---
+++ Prompt2App/src/test/java/com/example/Prompt2App/Prompt2AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prompt2App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prompt2AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
